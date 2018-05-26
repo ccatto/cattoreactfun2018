089b7e892e79720dccccee2a95f4df4734c84f59
Catto <catto@live.com>
2018-05-26 16:54:08 -0400

    className="width10per"
     className="width10per"

--- cattoreactappfunny2018may/src/Components/NavSecondRow.js
+++ cattoreactappfunny2018may/src/Components/NavSecondRow.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-
+import './SimpleStyle.css';
 
 class NavSecondRow extends Component {
-    render() {
-      return (
-        <div>
-          2nd row Nav
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <h3>2nd row Nav</h3>
+        <ul className="listStyleTypeNone ulFlex">
+         <li className="width15per">Logo
+              </li>
+          <li className="width65per">
+            <input className="width100" />
+          </li>
+          <li className="width10per">Account
+                  </li>
+          <li className="width10per"> Cart
+                    </li>
+        </ul>
+
+      </div>
+    );
   }
-  
-  export default NavSecondRow;
+}
+
+export default NavSecondRow;
