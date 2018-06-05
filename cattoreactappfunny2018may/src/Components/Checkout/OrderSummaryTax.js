e65733708bd31627495e2bb43350f7de54c2b01c
Catto, Chris <Christopher.Catto@newellco.com>
2018-06-05 15:41:18 -0400

    Updated Cart button text and image
    finnaly updated the style so the image is centered vertically. Whooha!

--- cattoreactappfunny2018may/src/Components/Checkout/OrderSummaryTax.js
+++ cattoreactappfunny2018may/src/Components/Checkout/OrderSummaryTax.js
@@ -3,7 +3,7 @@ import CartPromoCode from './CartPromoCode.js';
 import '../SimpleStyle.css';
 import './Cart.css';
 import buttonCounter from '../Utils/buttonCounter.js';
-
+import NButtonOrderSummary from '../Utils/NButtonOrderSummary.js';
 
 class OrderSummaryTax extends Component {
 
@@ -241,28 +241,7 @@ class OrderSummaryTax extends Component {
           <li className="marginTop15px fontsize22px"><b>$149.98</b>
           </li>
         </ul>
-        <ul className="flexRowNoWrap marginTop10px">
-          <li className="width5per">
-          </li>
-          <li className="width90per minHeight45px">
-            <button className="buttonBlue">
-              <ul className="flexRowNoWrap minHeight45px">
-                <li className="minWidth25per minHeight45px">
-                </li>
-                <li className="width70per minHeight45px textAlignLeft">
-                  <ul className="flexRowNoWrap minHeight45px">
-                    <li className="width10per minHeight45px justifyContentFlexEnd">
-                      <img className="square25" src="https://www.intel.com/content/dam/www/public/us/en/images/distributor/distributor-icon-lock-white-rwd.png.rendition.intel.web.256.192.png" />
-                    </li>
-                    <li className="width70per minHeight45px">
-                      <span className="colorGrayLight">Continue to Shipping</span>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </button>
-          </li>
-        </ul>
+        <NButtonOrderSummary />
         <hr className="width90per marginTop15px" />
         <CartPromoCode />
         <hr className="width90per" />
---
+++ cattoreactappfunny2018may/src/Components/Utils/NButtonOrderSummary.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../SimpleStyle.css';
+
+class NButtonOrderSummary extends Component {
+    render() {
+        return (
+            <button className="buttonBlue width90per marginTop10px">
+                <img className="square25 verticalAlignMiddle" src="https://www.intel.com/content/dam/www/public/us/en/images/distributor/distributor-icon-lock-white-rwd.png.rendition.intel.web.256.192.png" />
+                Continue to Shopping
+            </button>
+        );
+    }
+}
+
+export default NButtonOrderSummary;
