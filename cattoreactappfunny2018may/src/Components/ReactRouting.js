ef5e3eccc18afd0c7d7027a8f80d2208ffb394eb
Catto, Chris <Christopher.Catto@newellco.com>
2018-07-20 12:50:51 -0400

    altereed shipping and added a route

--- cattoreactappfunny2018may/src/Components/ReactRouting.js
+++ cattoreactappfunny2018may/src/Components/ReactRouting.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Home from './Home.js';
-import ToDoList from './GeneralSections/ToDoList.js'
-import Account from './Account/Account.js'
-import Cart from './Checkout/Cart.js'
-import Live from './GeneralSections/Live.js';
-import Learn from './GeneralSections/Learn.js';
-import Work from './GeneralSections/Work.js';
-import Play from './GeneralSections/Play.js';
+import Home from "./Home.js";
+import ToDoList from "./GeneralSections/ToDoList.js";
+import Account from "./Account/Account.js";
+import Cart from "./Checkout/Cart.js";
+import Live from "./GeneralSections/Live.js";
+import Learn from "./GeneralSections/Learn.js";
+import Work from "./GeneralSections/Work.js";
+import Play from "./GeneralSections/Play.js";
+import Shipping from "./Checkout/Shipping/Shipping.js";
 
 class ReactRouting extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path='/' component={Home} />
-          <Route path='/account' component={Account} />
-          <Route path='/cart' component={Cart} />
-          <Route path='/todolist' component={ToDoList} />
-          <Route path='/live' component={Live} />
-          <Route path='/learn' component={Learn} />
-          <Route path='/work' component={Work} />
-          <Route path='/play' component={Play} />
+          <Route exact path="/" component={Home} />
+          <Route path="/account" component={Account} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/todolist" component={ToDoList} />
+          <Route path="/live" component={Live} />
+          <Route path="/learn" component={Learn} />
+          <Route path="/work" component={Work} />
+          <Route path="/play" component={Play} />
+          <Route path="/shipping" component={Shipping} />
         </div>
       </Router>
     );
