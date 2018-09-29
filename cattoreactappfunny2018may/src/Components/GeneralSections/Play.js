363041d16bd2be453616a35bb34820118ea38b4a
Chris Catto <christopher.catto@newellco.com>
2018-09-29 11:18:06 -0400

    some alters

--- cattoreactappfunny2018may/src/Components/GeneralSections/Play.js
+++ cattoreactappfunny2018may/src/Components/GeneralSections/Play.js
@@ -1,6 +1,59 @@
 import React, { Component } from 'react';
 
 class Play extends Component {
+
+
+  constructor(props) {
+
+    super(props);
+    this.state = { playFun: "playFun" };
+  }
+
+  componentDidMount() {
+    console.log("here inside play");
+
+
+
+    // grant_type=password&username=john.doe@ep.com&password=password&scope=newell&role=REGISTERED
+
+
+    
+    // $('#oAuthSubmit').click(function(event){
+    //   // get relevant form variables
+    //   var userName = $('#oAuthUserName').val();
+    //   var userPassword = $('#oAuthPassword').val();
+    //   var SCOPE = 'mobee';
+    //   var ROLE = 'REGISTERED';
+    //   // assemble message form of post
+    //   var authString = 'grant_type=password&';
+    //     authString += 'username=' + userName + '&password=' + userPassword;
+    //     authString += '&scope=' + SCOPE + '&role=' + ROLE;
+      
+    //   // post to server
+    //  $.ajax({
+    //     type: 'POST',
+    //     url: '/cortex/oauth2/tokens',
+    //     contentType: 'application/x-www-form-urlencoded;charset=utf-8',
+    //     data: authString,
+    //     success:function(json, responseStatus, xhr){
+    //       // auth header value to be used in subsequent request headers
+    //       // this value should be persisted (localStorage) and returned with
+    //       // each request
+    //       var authReqHeaderString = 'Bearer ' + json.access_token;
+    //     },
+    //     error:function(response){
+    //       // handle exceptions from Cortex:
+    //       // handle bad credentials
+    //       // handle missing username/password
+    //     }
+    //   });
+
+    // end of button click
+    // });
+
+    console.log("end of compenentDidMount");
+  }
+
   render() {
     return (
       <div className="marginTop15px">
